eb17f3eb535db4356e3f10c7a29449f696a9e9d4
shashankg361 <shashankg361@gmail.com>
2024-09-25 12:29:36 +0530

    First-commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Prime
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Enter number");
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine("The given number is: "+PrimeOrNot(num));
+    }
+
+    public static String PrimeOrNot(int num)
+    {
+        for(int i = 2; i < num; i++)
+        {
+            if(num % i == 0 && i!=num)
+            {
+                return "Not Prime";
+            }
+        }
+        return "Prime";
+    }
+}
